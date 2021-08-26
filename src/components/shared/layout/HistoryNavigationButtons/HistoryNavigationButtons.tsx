ae2578ad228d583fdd30273b30d518a1becd725b
darylduck <daryl.duckmanton@gmail.com>
2021-08-26 20:42:20 +1000

    Refactored some components

--- src/components/shared/layout/HistoryNavigationButtons/HistoryNavigationButtons.tsx
+++ src/components/shared/layout/HistoryNavigationButtons/HistoryNavigationButtons.tsx
@@ -1,15 +1,21 @@
 import style from './HistoryNavigationButtons.module.scss';
 
 const HistoryNavigationButtons = (props: any) => {
+    let leftArrowClassName = 'arrow-icon';
+    let rightArrowClassName = 'arrow-icon';
+
+    leftArrowClassName += props.backButtonDisabled ? ' disabled': '';
+    rightArrowClassName += props.forwardButtonDisabled ? ' disabled': '';
+
     return (
         <div id={style.historyNavigationButtons}>
-            <button disabled={props.backButtonDisabled}> {/* [disabled]="backButtonDisabled" (click)="navigateBack()" */}
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" className="arrow-icon"> {/*  [class.disabled]="disabled" */}
+            <button disabled={props.backButtonDisabled}> {/* (click)="navigateBack()" */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" className={leftArrowClassName}>
                     <path d="M4.31,11.23l10.4-8.92.69.76L5.81,11.23l9.55,8.16-.65.76" />
                 </svg>
             </button>
-            <button className={style.forwardButton} disabled={props.forwardButtonDisabled}> {/* [disabled]="forwardButtonDisabled" (click)="navigateForward()" */}
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" className="arrow-icon"> {/*  [class.disabled]="disabled" */}
+            <button className={style.forwardButton} disabled={props.forwardButtonDisabled}> {/* (click)="navigateForward()" */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" className={rightArrowClassName}>
                     <path d="M8,21.15l-.65-.76,9.55-8.16L7.31,4.07,8,3.31,18.4,12.23"></path>
                 </svg>
             </button>
