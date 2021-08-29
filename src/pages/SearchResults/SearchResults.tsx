8538f4c010a0d8c50e9c68d0b0d7888a97dd3d22
darylduck <daryl.duckmanton@gmail.com>
2021-08-29 15:09:13 +1000

    Started search results

--- src/pages/SearchResults/SearchResults.tsx
+++ src/pages/SearchResults/SearchResults.tsx
@@ -1,10 +1,33 @@
+import { useEffect } from "react";
+
+export interface Song {
+    title: string;
+}
+
+
 const SearchResults = (props: any) => {
+    const searchTerm = props?.match?.params?.searchTerm;
+    const results: Array<Song> = [{
+        title: 'Rudix World'
+    }];    
+    const resultsEl = results.length === 0 ? (
+        <p>
+            No results found for &quot;{searchTerm}&quot;
+        </p>
+    ): results.map(s => (
+        <p>
+            {s.title}
+        </p>
+    ));
+
+    useEffect(() => {
+
+    }, [searchTerm]);    
+
     return (
         <section className="start-search">
             <h2>Search Results</h2>
-            <p>
-                No results found for "{props?.match?.params?.searchTerm}"
-            </p>
+            {resultsEl}
         </section>
     );
 }
