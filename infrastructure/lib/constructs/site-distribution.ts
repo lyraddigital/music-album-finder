d0531a93ebe469eed8d75cff84ab051a8d76c5d2
lyraddigital <daryl@lyraddigital.com>
2021-09-07 21:35:09 +1000

    Update site-distribution.ts

--- infrastructure/lib/constructs/site-distribution.ts
+++ infrastructure/lib/constructs/site-distribution.ts
@@ -16,22 +16,22 @@ export class SiteDistribution extends Construct {
     constructor(parent: Construct, id: string, props: SiteDistributionProps) {
         super(parent, id);
         
-        const zone = HostedZone.fromLookup(this, 'Zone', { domainName: props.rootDomain });
+        /*const zone = HostedZone.fromLookup(this, 'Zone', { domainName: props.rootDomain });
 
         const certificate = new DnsValidatedCertificate(this, 'WebsiteCertificate', {
             domainName: props.rootDomain,
             validation: CertificateValidation.fromDns(),
             subjectAlternativeNames: [`${props.subDomain}.${props.rootDomain}`],
             hostedZone: zone
-        });
+        });*/
 
         this.instance = new CloudFrontWebDistribution(this, 'WebsiteDistribution', {
-            aliasConfiguration: {
-                acmCertRef: certificate.certificateArn,
-                names: [ props.rootDomain ],
-                sslMethod: SSLMethod.SNI,
-                securityPolicy: SecurityPolicyProtocol.TLS_V1_1_2016,
-            },
+            //aliasConfiguration: {
+                // acmCertRef: certificate.certificateArn,
+                // names: [ props.rootDomain ],
+                // sslMethod: SSLMethod.SNI,
+                // securityPolicy: SecurityPolicyProtocol.TLS_V1_1_2016,
+            //},
             originConfigs: [
                 {
                     s3OriginSource: {
