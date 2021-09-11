a276653285622194071a372ecff529c125d1f112
darylduck <daryl.duckmanton@gmail.com>
2021-09-11 20:37:37 +1000

    Added Stack name per environment

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -4,18 +4,8 @@ import * as cdk from '@aws-cdk/core';
 import { WebsiteStack } from '../lib/website-stack';
 
 const app = new cdk.App();
-new WebsiteStack(app, 'SpotifyCloneWebsiteStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new WebsiteStack(app, 'SpotifyCloneWebsiteStack-Development', { env });
+new WebsiteStack(app, 'SpotifyCloneWebsiteStack-Testing', { env });
+new WebsiteStack(app, 'SpotifyCloneWebsiteStack-Production', { env });
