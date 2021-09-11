441c359046992e739ca2178548b199d1aa331a23
darylduck <daryl.duckmanton@gmail.com>
2021-09-11 13:41:24 +1000

    Made the subdomain for the CDK project available via Github environment variables.

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -4,7 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import { WebsiteStack } from '../lib/website-stack';
 
 const app = new cdk.App();
-new WebsiteStack(app, 'WebsiteStack', {
+new WebsiteStack(app, 'SpotifyCloneWebsiteStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
