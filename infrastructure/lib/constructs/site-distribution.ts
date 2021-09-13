0d6a7597c9a6edae94a503d5f417322dd65ab4e3
darylduck <daryl.duckmanton@gmail.com>
2021-09-13 16:06:03 +1000

    Checking

--- infrastructure/lib/constructs/site-distribution.ts
+++ infrastructure/lib/constructs/site-distribution.ts
@@ -16,6 +16,7 @@ export class SiteDistribution extends Construct {
     constructor(parent: Construct, id: string, props: SiteDistributionProps) {
         super(parent, id);
 
+        console.log('Sub Domain Input: ', props.subDomain);
         console.log('Root Domain Input: ', props.rootDomain);
         
         const zone = HostedZone.fromLookup(this, 'Zone', { domainName: props.rootDomain });
