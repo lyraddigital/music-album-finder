4cccd65f6f7ccbc936852422ad3bf6a52cf56aa1
darylduck <daryl.duckmanton@gmail.com>
2021-09-15 16:11:11 +1000

    Added Staging Stack

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -7,5 +7,5 @@ const app = new cdk.App();
 const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
 
 new WebsiteStack(app, 'SpotifyCloneWebsiteStack-Development', { env });
-new WebsiteStack(app, 'SpotifyCloneWebsiteStack-Testing', { env });
+new WebsiteStack(app, 'SpotifyCloneWebsiteStack-Staging', { env });
 new WebsiteStack(app, 'SpotifyCloneWebsiteStack-Production', { env });
